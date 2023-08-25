f8cd8c252b78f86defb14b20b87aadc09cc5e7de
EneaK9 <eneakuca0@gmail.com>
2023-08-25 16:21:06 +0200

    First Commit

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header>
+      <Image
+        src="https://links.papareact.com/c2cdd5"
+        alt="Trello Logo"
+        width={300}
+        height={100}
+        className="w-44 md:w-56 pb-10 md:pb-9 object-contain"
+       />{" "}
+    </header>
+  );
+}
+
+export default Header;
